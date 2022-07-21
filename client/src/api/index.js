1c934b56a5063e00619b51211118e5401a56c1e6
Brian Harsha <harshabc4@gmail.com>
2022-07-21 10:39:52 -0400

    cleanup old files

--- client/src/api/index.js
+++
@@ -1,5 +0,0 @@
-// import axios from "axios";
-
-// const url = "/showMaterial";
-
-// export const fetchMaterials = () => axios.get(url);
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,29 @@
+import "./Navbar.css";
+
 function Navbar() {
   return (
     <div className="Navbar">
-      <nav>
-        <div>
-          <a href="/">FinishLine</a>
+      <header>
+        <nav>
+          <div>
+            <a href="/">FinishLine</a>
 
-          <div id="navmenu">
-            <ul>
-              <li>
-                <a href="/materials">Projects</a>
-              </li>
-              <li>
-                <a href="#">Account</a>
-              </li>
-              <li>
-                <a href="#">Search</a>
-              </li>
-            </ul>
+            <div id="navmenu">
+              <ul>
+                <li>
+                  <a href="/materials">Projects</a>
+                </li>
+                <li>
+                  <a href="#">Account</a>
+                </li>
+                <li>
+                  <a href="#">Search</a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </header>
     </div>
   );
 }
